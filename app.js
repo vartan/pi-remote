a5d9c769b555b0f746cae3a1a482e38ff67b7923
Michael Vartan <michael.vartan@student.csulb.edu>
2014-11-25 19:33:13 -0800

    better documentation

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
+/**
+ * app.js
+ * 
+ * IR Remote Web Service
+ * 
+ * @author Michael Vartan
+ * @version 1.0.0 
+ */
+
 var express = require('express');
 var app = express();
 var sys = require('sys')
 var exec = require('child_process').exec;
 
-
+/**
+ * Dictionary of devices and their buttons
+ * @type {Object}
+ */
 var devices = {};
+/**
+ * Generates function to get devices' buttons from irsend command
+ * @param  {String} deviceName name of device
+ * @return {Function}            exec callback
+ */ 
 var getCommandsForDevice = function(deviceName) {
+  /**
+   * Get Device's Button from irsend command
+   * @param  {String} error  Error from running command
+   * @param  {String} stdout std out
+   * @param  {String} stderr std err
+   * @return {None}        
+   */
   return function(error, stdout, stderr) {
     var lines = stderr.split("\n");
     for(var lineIndex in lines) {
@@ -19,6 +43,13 @@ var getCommandsForDevice = function(deviceName) {
     }
   }
 };
+/**
+ * Get Device from irsend command
+ * @param  {String} error  Error from running command
+ * @param  {String} stdout std out
+ * @param  {String} stderr std err
+ * @return {None}        
+ */
 var getDevice = function (error, stdout, stderr) {
   if(error) {
     console.log("irsend not available.");
@@ -37,31 +68,51 @@ var getDevice = function (error, stdout, stderr) {
     }
   }          
 };
+// Get all device information
 exec("irsend list \"\" \"\"", getDevice);
-app.use(express.static(__dirname + '/html')); //where /html is a subfolder of your app 
+
+// Define static HTML files
+app.use(express.static(__dirname + '/html'));
+
+
+
+// define GET request for /send/deviceName/buttonName
 app.get('/send/:device/:key', function(req, res) {
+
   var deviceName = req.param("device");
   var key = "KEY_"+req.param("key").toUpperCase();
-  var keyNumber = ~~key;
+
+  // Make sure that the user has requested a valid device 
   if(!devices.hasOwnProperty(deviceName)) {
     res.send("invalid device");
     return;
   }
+
+  // Make sure that the user has requested a valid key/button
   var device = devices[deviceName];
-  var inArray = false;
+  var deviceKeyFound = false;
   for(var i = 0; i < device.length; i++) {
     if(device[i] == key) {
-      inArray = true; 
+      deviceKeyFound = true; 
       break;
     }
   }
-  if(!inArray) {
+  if(!deviceKeyFound) {
     res.send("invalid key number: "+key);
     return;
   }
-  
+
+  // send command to irsend
   var command = "irsend SEND_ONCE "+deviceName+" "+key;
-  exec(command, function(a,b,c){});
-  res.send("valid");
-});
+  exec(command, function(error, stdout, stderr){
+    if(error)
+      res.send("Error sending command");
+    else   
+      res.send("Successfully sent command");
+  });
+
+
+}); // end define GET request for /send/deviceName/buttonName
+
+// Listen on port 80
 app.listen('80');
